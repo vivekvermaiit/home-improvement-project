705a9e71f4806347fe910c1c971bbe5a510a4046
Vivek Verma <vivekvermaiit@gmail.com>
2019-12-21 21:11:38 -0500

    fixing migration

--- db/migrate/20191221155738_create_default_roles_and_users.rb
+++ db/migrate/20191221155738_create_default_roles_and_users.rb
@@ -1,11 +1,11 @@
 class CreateDefaultRolesAndUsers < ActiveRecord::Migration[5.1]
   def self.up
-
-    admin_user =  User.admin_user
-
-    admin_user ||= User.create!(:email=>User::ADMIN[:email],:password=>User::ADMIN[:password])
     admin_role = Role.find_or_create_by(:name => Role::ROLES[:admin])
     member_role = Role.find_or_create_by(:name => Role::ROLES[:member])
+
+    admin_user =  User.admin_user
+    admin_user ||= User.create!(:email=>User::ADMIN[:email],:password=>User::ADMIN[:password])
+
     admin_user.role = admin_role
     admin_user.save!
   end
